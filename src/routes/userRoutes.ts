09431e94319a528a5c77efd161d930a18a7c47c7
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-02-04 23:56:17 +0330

    fix: add authentication middleware fo the profile api

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { getProfile } from "../controllers/userController";
+import { authenticateToken } from "../middleware";
 
 const router = express.Router();
 
-router.get("/profile", getProfile);
+router.get("/profile", authenticateToken, getProfile);
 
 export default router;
